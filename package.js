a6d61839f647685c9b9992146ead3ca30b5145f3
Josh <josh.bechard@gmail.com>
2015-02-13 16:52:50 -0600

    Change bootstrap package
    Changed this to the official twbs:bootstrap package

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
     summary: "DataTable - jQuery plugin for sortable, pagable data",
     name: "j4507:datatables-bootstrap-3",
-    version: "0.2.3",
+    version: "0.2.4",
     git: "https://github.com/c316/meteor-datatables-bootstrap-3"
 });
 
 Package.on_use(function (api) {
   api.use('jquery@1.0.0', 'client');
-  api.use('mizzao:bootstrap-3@3.2.0', 'client');
+  api.use('twbs:bootstrap', 'client');
   api.add_files([
     'lib/jquery.dataTables.js',
     'lib/datatables.js',
